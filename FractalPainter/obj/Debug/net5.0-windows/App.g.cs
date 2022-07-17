951c336bde9f935656fa407fbaec0998407f8f0d
Yanulio <yagshulepa@edu.hse.ru>
2022-07-17 19:05:24 +0300

    Edited README.md file

--- FractalPainter/obj/Debug/net5.0-windows/App.g.cs
+++ FractalPainter/obj/Debug/net5.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace FractalPainter {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace FractalPainter {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             FractalPainter.App app = new FractalPainter.App();
             app.InitializeComponent();
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # FractalPainter
-WPF Application for drawing fractals
+WPF Application for drawing fractals.
+
+![AppView](images/AppView.png)
+
+# Features
+
+* Drawing five types of fractals, such as:
+  * Pythagoras tree
+  * Koch curve
+  * Sierpinski carpet
+  * Sierpinski triangle
+  * Cantor set
+* Changing the recursion depth of fractals.
+* For Pythagoras tree: changing left and right angle value, length ratio.
+* For Cantor set: changing distance between each level.
+* Saving an image of current fractal as a .png file.
